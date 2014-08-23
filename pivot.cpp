117ff6ebfa18222dc2a500a468ee6d232cc27bfe
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-23 09:34:48 -0700

    Cleaned up buildResults() by removing necessity for some loop logic.

--- pivot.cpp
+++ pivot.cpp
@@ -10,28 +10,21 @@
 #include<fstream>
 #include<string>
 
-using namespace std;
-
 struct results{
   int terminal[10];
-  string day[7];
+  std::string day[7];
   int kWh[10][7];
 };
 
-results buildResults(int i, int j){
+results buildResults(int i){
   results res={{1000,1001,1002,1003,1004,1005,1006,1007,1008,1009},
   {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"},0};
   int terminal,kWh;
-  string day;
-  ifstream infile("windfarm.dat");
+  std::string day;
+  std::ifstream infile("windfarm.dat");
   while(infile>>terminal>>day>>kWh){
-    for(i=0;i<sizeof(res.terminal)/sizeof(res.terminal[0]);i++){
-      for(j=0;j<sizeof(res.day)/sizeof(res.day[0]);j++){
-        if(terminal==res.terminal[i]&&!day.compare(res.day[j])){
-          res.kWh[i][j]+=kWh;
-        }
-      }
-    }
+    for(i=0;day.compare(res.day[i]);i++);
+    res.kWh[terminal-1000][i]+=kWh;
   }
   infile.close();
   return res;
@@ -39,21 +32,21 @@ results buildResults(int i, int j){
 
 void printChart(results res,int i, int j){
   for(i=0;i<7;i++){
-    cout<<"\t"<<res.day[i];
+    std::cout<<"\t"<<res.day[i];
   }
-  cout<<endl;
+  std::cout<<'\n';
   for(i=0;i<sizeof(res.terminal)/sizeof(res.terminal[0]);i++){
-    cout<<res.terminal[i]<<"\t";
+    std::cout<<res.terminal[i]<<"\t";
     for(j=0;j<sizeof(res.day)/sizeof(res.day[0]);j++){
-      cout<<res.kWh[i][j]<<"kWh\t";
+      std::cout<<res.kWh[i][j]<<"kWh\t";
     }
-    cout<<endl;
+    std::cout<<'\n';
   }
 }
 
 int main(void){
   int i,j;
-  results res = buildResults(i, j);
+  results res = buildResults(i);
   printChart(res, i, j);
   return 0;
 }
